8a94af4b649e056d3a0984176d34fb44ee35ae93
nicolas-lortie <nic.lortie@gmail.com>
2014-10-20 19:46:43 -0400

    adding project structure and lists

---
+++ bin/ex08.dart
@@ -0,0 +1,23 @@
+import '../lib/functions.dart';
+
+void main(){
+  
+  List associations = [
+   {"name":"sp", "description":"South Park", "members":
+    [{"firstname":"Eric", "lastname":"Cartman", "e-mail":"ec@gmail.com"},
+    {"firstname":"Kyle", "lastname":"Broflovski", "e-mail":"kb@gmail.com"},
+    {"firstname":"Stan", "lastname":"Marsh", "e-mail":"sm@gmail.com"},
+    {"firstname":"Kenny", "lastname":"Mccormick", "e-mail":"km@gmail.com"},
+    {"firstname":"Randy", "lastname":"Marsh", "e-mail":"rm@gmail.com"},
+    {"firstname":"Herbert", "lastname":"Garrison", "e-mail":"hg@gmail.com"}]},
+   {"name":"bbt", "description":"Big bang Theory", "members":
+     [{"firstname":"Rajesh", "lastname":"koothrappali", "e-mail":"rk@gmail.com"},
+      {"firstname":"Sheldon", "lastname":"Cooper", "e-mail":"sp@gmail.com"},
+      {"firstname":"Howard", "lastname":"Wolowitz", "e-mail":"hw@gmail.com"},
+      {"firstname":"Leonard", "lastname":"Hofstadter", "e-mail":"lh@gmail.com"}]}
+    ];
+  
+  
+
+
+}
---
+++ lib/functions.dart
@@ -0,0 +1,3 @@
+library ex08;
+
+
