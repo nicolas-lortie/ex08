1a839d3ea7a12242188930fab730b8a99f7aadd5
nicolas-lortie <nic.lortie@gmail.com>
2014-10-21 16:00:17 -0400

    Question 1

--- bin/ex08.dart
+++ bin/ex08.dart
@@ -1,23 +1,71 @@
 import '../lib/functions.dart';
 
-void main(){
-  
-  List associations = [
-   {"name":"sp", "description":"South Park", "members":
-    [{"firstname":"Eric", "lastname":"Cartman", "e-mail":"ec@gmail.com"},
-    {"firstname":"Kyle", "lastname":"Broflovski", "e-mail":"kb@gmail.com"},
-    {"firstname":"Stan", "lastname":"Marsh", "e-mail":"sm@gmail.com"},
-    {"firstname":"Kenny", "lastname":"Mccormick", "e-mail":"km@gmail.com"},
-    {"firstname":"Randy", "lastname":"Marsh", "e-mail":"rm@gmail.com"},
-    {"firstname":"Herbert", "lastname":"Garrison", "e-mail":"hg@gmail.com"}]},
-   {"name":"bbt", "description":"Big bang Theory", "members":
-     [{"firstname":"Rajesh", "lastname":"koothrappali", "e-mail":"rk@gmail.com"},
-      {"firstname":"Sheldon", "lastname":"Cooper", "e-mail":"sp@gmail.com"},
-      {"firstname":"Howard", "lastname":"Wolowitz", "e-mail":"hw@gmail.com"},
-      {"firstname":"Leonard", "lastname":"Hofstadter", "e-mail":"lh@gmail.com"}]}
-    ];
-  
-  
+void main() {
+
+  List members = [{
+      "TvShow": "South Park",
+      "FirstName": "Eric",
+      "LastName": "Cartman",
+      "e-mail": "ec@gmail.com"
+    }, {
+      "TvShow": "South Park",
+      "FirstName": "Kyle",
+      "LastName": "Broflovski",
+      "e-mail": "kb@gmail.com"
+    }, {
+      "TvShow": "South Park",
+      "FirstName": "Stan",
+      "LastName": "Marsh",
+      "e-mail": "sm@gmail.com"
+    }, {
+      "TvShow": "South Park",
+      "FirstName": "Kenny",
+      "LastName": "Mccormick",
+      "e-mail": "km@gmail.com"
+    }, {
+      "TvShow": "South Park",
+      "FirstName": "Randy",
+      "LastName": "Marsh",
+      "e-mail": "rm@gmail.com"
+    }, {
+      "TvShow": "South Park",
+      "FirstName": "Herbert",
+      "LastName": "Garrison",
+      "e-mail": "hg@gmail.com"
+    }, {
+      "TvShow": "Big Bang Theory",
+      "FirstName": "Rajesh",
+      "LastName": "koothrappali",
+      "e-mail": "rk@gmail.com"
+    }, {
+      "TvShow": "Big Bang Theory",
+      "FirstName": "Sheldon",
+      "LastName": "Cooper",
+      "e-mail": "sc@gmail.com"
+    }, {
+      "TvShow": "Big Bang Theory",
+      "FirstName": "Howard",
+      "LastName": "Wolowitz",
+      "e-mail": "hw@gmail.com"
+    }, {
+      "TvShow": "Big Bang Theory",
+      "FirstName": "Leonard",
+      "LastName": "Hofstadter",
+      "e-mail": "lh@gmail.com"
+    }];
+
+  var sp = ('South Park');
+  print('\nQuestion 1.1 members from $sp sorted by last name\n');
+  print(sortByLastName(members, sp));
+
+  var bbt = ("Big Bang Theory");
+  print('\nQuestion 1.2 members from $bbt sorted by first name\n');
+  print(sortByFirstName(members, bbt));
+
+  var letter = ('C');
+  print('\nQuestion 1.3 members with last name starting by the letter $letter\n');
+  print(nameStartByC(members, letter));
+
 
 
 }
--- lib/functions.dart
+++ lib/functions.dart
@@ -1,3 +1,62 @@
 library ex08;
 
+// Question 1.1: Sort the list of members of an association by the last name
+// Code inspiration from Stackoverflow
+// http://stackoverflow.com/questions/12888206/how-can-i-sort-a-array-list-of-strings-in-dart
+
+
+/* For the question 1.1, I will sort by the last name
+ * every characters from one association
+ * while ignoring members from other associations.
+ */
+
+sortByLastName(var members, association) {
+  List associationMembers = new List();
+
+  for (var j in members) {
+    if (j["TvShow"] == association) associationMembers.add(j);
+  }
+  associationMembers.sort((a, b) => a["LastName"].compareTo(b["LastName"]));
+  for (var elmt in associationMembers) {
+    print(elmt);
+  }
+  return ('');
+}
+
+
+
+// Question 1.2: Sort the list of members of an association by the first name
+
+
+sortByFirstName(var members, var association) {
+  List associationMembers = new List();
+
+  for (var j in members) {
+    if (j["TvShow"] == association) associationMembers.add(j);
+  }
+  associationMembers.sort((a, b) => a["FirstName"].compareTo(b["FirstName"]));
+  for (var elmt in associationMembers) {
+
+    print(elmt);
+  }
+  return ('');
+}
+
+
+// Question 1.3: Show members with last name starting by a given letter
+// Added code so that members names print sorted by first name (same code from question 1.1)
+
+
+nameStartByC(var members, var letter) {
+  List membersLetter = new List();
+
+  for (var j in members) {
+    if (j['LastName'][0] == letter) membersLetter.add(j);
+  }
+  membersLetter.sort((a, b) => a["FirstName"].compareTo(b["FirstName"]));
+  for (var elmt in membersLetter) {
+    print(elmt);
+  }
+  return ('');
+}
 
